907ec8cfbb38f96d65738b46b60f15ac778679ff
Victor Lee <myleevictor@gmail.com>
2018-08-31 14:49:40 +0800

    Component rendering via JSX or react.createElement and ternary

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function MyComponent() {
+  return (
+    <div className="book">
+      <div className="title">The Title</div>
+      <div className="author">The Author</div>
+      <ul className="stats">
+        <li className="rating">5 stars</li>
+        <li className="isbn">12-388880-910</li>
+      </ul>
+    </div>
+  );
+}
+
+function AnotherWayComponent() {
+  return React.createElement(
+    'div',
+    { className: 'book' },
+    React.createElement('div', { className: 'title' }, 'The Title'),
+    React.createElement('div', { className: 'author' }, 'The Author'),
+    React.createElement(
+      'ul',
+      { className: 'stats' },
+      React.createElement('li', { className: 'rating' }, '5 stars'),
+      React.createElement('li', { className: 'isbn' }, '12-388880-910')
+    )
+  );
+}
+
+function Greeting() {
+  var username = "Victor";
+ // try undefine, null, true, 
+  return (
+    <span>
+      {username ? "Hello "+ username : 'Not logged in.'}
+    </span>
+  )
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
